b5fcd503aad73a7ae8cd7f4513aa0cf26c108464
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2021-03-31 14:01:22 +0530

    Create calendar.py

---
+++ calendar.py
@@ -0,0 +1,17 @@
+import calendar
+year = int(input("Enter the year : "))
+month = int(input("Enter the month : "))
+print(calendar.month(year, month))
+
+
+#OUTPUT:
+Enter the year : 2001
+Enter the month : 1
+    January 2001
+Mo Tu We Th Fr Sa Su
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30 31
+  
